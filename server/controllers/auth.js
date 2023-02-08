6b3b58549f90d8a365b66ca2e4f742f5db545633
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-09 00:39:39 +0400

    password privacy has been created

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,16 +1,16 @@
-import User from "../models/User.js"
-
-export const register= async(req,res,next)=>{
-    try{
-        const newUser=new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:req.body.password,
-        })
-        await newUser.save(
-            res.status(200).send("User has been created.")
-        )
-    }catch(err){
-        next(err)
-    }
-}
+import User from "../models/User.js";
+import bcrypt from "bcryptjs"
+export const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
+    await newUser.save(res.status(200).send("User has been created."));
+  } catch (err) {
+    next(err);
+  }
+};
