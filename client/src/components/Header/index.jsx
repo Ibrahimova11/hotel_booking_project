086ee9060e65f0cc425ae092a9568acd2d794a77
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-11 01:15:37 +0400

    Header part created

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,10 +1,49 @@
+import React from "react";
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faCalendarDays,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
 
-import React from 'react'
-import styled from './style.module.css'
 const Index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <div className="header">
+      <div className="header__container">
+        <div className="header__list">
+          <div className="header__list-item active ">
+            <FontAwesomeIcon icon={faBed} />
+            <span className="header__list-item-text">Stays</span>
+          </div>
+          <div className="header__list-item">
+            <FontAwesomeIcon icon={faPlane} />
+            <span className="header__list-item-text">Flights</span>
+          </div>
+          <div className="header__list-item">
+            <FontAwesomeIcon icon={faCar} />
+            <span className="header__list-item-text">Car rentals</span>
+          </div>
+          <div className="header__list-item">
+            <FontAwesomeIcon icon={faBed} />
+            <span className="header__list-item-text">Attractions</span>
+          </div>
+          <div className="header__list-item">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span className="header__list-item-text">Airport taxis</span>
+          </div>
+        </div>
+        <h1 className="header__title">A lifetime of discounts? It's Genius.</h1>
+<p className="header__desc">
+  Get rewarded for your travels – unlock instant savings of 10% or more with a free Lamabooking account
+</p>
+<button className="header__btn">Sign in / Register</button>
+      </div>
+    </div>
+  );
+};
 
-export default Index
+export default Index;
