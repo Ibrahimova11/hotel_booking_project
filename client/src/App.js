8aec480dff0acf7a78fadb8179eea077b6a7041e
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-10 18:51:35 +0400

    Home , Hotel, List folders and all routers created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Hotel from "./pages/Hotel";
+import List from "./pages/List";
 
 function App() {
   return (
-   <div>salam</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
