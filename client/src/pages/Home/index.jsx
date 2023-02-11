0a34bc368bcfad8630f24f944911e702ebeceb6f
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-12 01:38:27 +0400

    Our PropertyList is also finished

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import "./style.css"
 import Header  from "../.././components/Header"
 import Featured from "../../components/Featured"
+import PropertyList from "../../components/PropertyList"
 const Home = () => {
   return (
     <div>
     <Header />
     <div className="home__container">
     <Featured/>
-    
+    <h1 className="home__title">Browse by property type</h1>
+    <PropertyList/>
     </div>
     </div>
   )
