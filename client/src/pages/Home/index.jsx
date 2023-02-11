a6e095b90f8d9b19472d27e99f85c077d3218405
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-12 02:51:33 +0400

    Featured properties part finished

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import "./style.css"
 import Header  from "../.././components/Header"
 import Featured from "../../components/Featured"
 import PropertyList from "../../components/PropertyList"
+import FeaturedProperties from "../../components/FeaturedProperties"
 const Home = () => {
   return (
     <div>
@@ -11,6 +12,8 @@ const Home = () => {
     <Featured/>
     <h1 className="home__title">Browse by property type</h1>
     <PropertyList/>
+    <h1 className="home__title">Homes guests love</h1>
+    <FeaturedProperties/>
     </div>
     </div>
   )
