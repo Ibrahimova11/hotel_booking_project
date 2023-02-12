0eeccf070864e2adcf5c850976dc91da6ad023fa
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-12 04:41:41 +0400

    Footer finished

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Home from "./pages/Home";
 import Hotel from "./pages/Hotel";
 import List from "./pages/List";
 import Navbar from "./components/Navbar";
-
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
