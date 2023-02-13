a47b7a6b14f9333d06a63f3e005fc03d99d3aea9
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-13 13:41:39 +0400

    New folder created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import "./App.css";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import Destinations from "./pages/Destinations";
+import Detail from "./pages/Detail"
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/destinations" element={<Destinations/>} />
+        <Route path="/detail/:id" element={<Detail/>} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
