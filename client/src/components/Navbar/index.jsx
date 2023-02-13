0b46495fee8cc3ca051116f8c9d8009d4cd96925
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-13 17:21:43 +0400

    The first Navbar finished

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 const Index = () => {
-  return (
-    <div>index</div>
-  )
-}
+  const [click, setClick] = useState(false);
 
-export default Index
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="container flex_space">
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : " fas fa-bars"}></i>
+          </div>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" onClick={closeMobileMenu}>
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link to="/gallery" onClick={closeMobileMenu}>
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link to="/destinations" onClick={closeMobileMenu}>
+                Destinations
+              </Link>
+            </li>
+            <li>
+              <Link to="/blog" onClick={closeMobileMenu}>
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link to="/testimonial" onClick={closeMobileMenu}>
+                Testimonial
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={closeMobileMenu}>
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+
+          <div className="login-area flex">
+            <li>
+              <Link to="/sign-in">
+                <i class="fa-solid fa-chevron-right"></i>Sign in
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <i class="fa-solid fa-chevron-right"></i>Register
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact">
+                <button className="primary-btn">Request a Quote</button>
+              </Link>
+            </li>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Index;
