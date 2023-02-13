0ad45002107a8e479d23b0be78aa1b72a49fce8b
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-13 17:51:41 +0400

    The second Navbar finished

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import Logo from "../../images/logo.png";
 
 const Index = () => {
   const [click, setClick] = useState(false);
@@ -72,6 +73,43 @@ const Index = () => {
           </div>
         </div>
       </nav>
+      <header>
+        <div className='container flex_space'>
+          <div className='logo'>
+            <img src={Logo} alt='' />
+          </div>
+
+          <div className='contact flex_space '>
+            <div className='box flex_space'>
+              <div className='icons'>
+              <i class="fa-regular fa-clock"></i>
+              </div>
+              <div className='text'>
+                <h4>Working Hours</h4>
+                <Link to='/contact'>Monday - Sunday: 9.00am to 6.00pm</Link>
+              </div>
+            </div>
+            <div className='box flex_space'>
+              <div className='icons'>
+              <i class='fas fa-phone-volume'></i>
+              </div>
+              <div className='text'>
+                <h4>Call Us</h4>
+                <Link to='/contact'>+011 123 4567</Link>
+              </div>
+            </div>
+            <div className='box flex_space'>
+              <div className='icons'>
+                <i class='far fa-envelope'></i>
+              </div>
+              <div className='text'>
+                <h4>Mail Us</h4>
+                <Link to='/contact'>info@exampal.com</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
