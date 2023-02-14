0954712e84452823462ffd8fdf8979d81d0f5ed6
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-15 03:25:39 +0400

    Testimonial data created

---
+++ client/src/api/TestimonialData.jsx
@@ -0,0 +1,46 @@
+const TestimonialData = [
+    {
+      id: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+      name: "KEVIN MARTHIN",
+      profile: "https://rainbowdesign.in/themes/Stetho/img/testimonial/01.jpg",
+      post: "Developer",
+    },
+    {
+      id: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+      name: "JOE GABRIEL",
+      profile: "https://rainbowdesign.in/themes/Stetho/img/testimonial/02.jpg",
+      post: "Designer",
+    },
+    {
+      id: 3,
+      desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+      name: "JOIN MANSOOR",
+      profile: "https://rainbowdesign.in/themes/Stetho/img/testimonial/03.jpg",
+      post: "Support-Marketing",
+    },
+    {
+      id: 4,
+      desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+      name: "SUSAN DAY",
+      profile: "https://rainbowdesign.in/themes/Stetho/img/testimonial/04.jpg",
+      post: "CEO",
+    },
+    {
+      id: 5,
+      desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+      name: "MICHAEL DAVID",
+      profile: "https://rainbowdesign.in/themes/Stetho/img/testimonial/05.jpg",
+      post: "Consultant",
+    },
+    {
+      id: 6,
+      desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+      name: "MEGAN MILLER",
+      profile: "https://rainbowdesign.in/themes/Stetho/img/testimonial/06.jpg",
+      post: "Founder",
+    },
+  ]
+  
+  export default TestimonialData
---
+++ client/src/pages/Testimonial/AllTestimonial.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllTestimonial = () => {
+  return (
+    <div>AllTestimonial</div>
+  )
+}
+
+export default AllTestimonial
