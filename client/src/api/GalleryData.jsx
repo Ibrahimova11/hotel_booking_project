680d52a6104ebaf800204f6a7b6d99aa0c124bb2
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-14 21:41:47 +0400

    Gallery page finished

---
+++ client/src/api/GalleryData.jsx
@@ -0,0 +1,28 @@
+const GalleryData = [
+    {
+      img: "https://rainbowdesign.in/themes/Stetho/img/gallery/1.jpg",
+      title: "Gallery One",
+    },
+    {
+      img: "https://rainbowdesign.in/themes/Stetho/img/gallery/2.jpg",
+      title: "Gallery Two",
+    },
+    {
+      img: "https://rainbowdesign.in/themes/Stetho/img/gallery/3.jpg",
+      title: "Gallery Three",
+    },
+    {
+      img: "https://rainbowdesign.in/themes/Stetho/img/gallery/4.jpg",
+      title: "Gallery Four",
+    },
+    {
+      img: "https://rainbowdesign.in/themes/Stetho/img/gallery/5.jpg",
+      title: "Gallery Five",
+    },
+    {
+      img: "https://rainbowdesign.in/themes/Stetho/img/gallery/6.jpg",
+      title: "Gallery Six",
+    },
+  ]
+  
+  export default GalleryData
--- client/src/pages/Gallery/index.jsx
+++ client/src/pages/Gallery/index.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from "react"
+import Card from "./Card"
+import "./style.css"
+import GalleryData from "../../api/GalleryData"
+import { Link, useLocation } from "react-router-dom"
 
+// Gallery page start
 const Index = () => {
+    const location = useLocation();
   return (
-    <div>index</div>
+    <>
+    <section className="image-heading">
+    <div className="container">
+      <h1>{location.pathname.split("/")[1]}</h1>
+
+      <button>
+        <Link to="/"> Home / </Link>
+        <span>{location.pathname.split("/")[1]}</span>
+      </button>
+    </div>
+  </section>
+
+      <section className='gallery top'>
+        <div className='container grid'>
+          {GalleryData.map((value) => {
+            return <Card imgaes={value.img} title={value.title} />
+          })}
+        </div>
+      </section>
+    </>
   )
 }
 
