33438ebc0525bc34a6f312259887f98b26784d0b
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-14 18:27:39 +0400

    Home page adedd section

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/HomeSection/Hero'
+import HomeAbout from '../../components/HomeSection/HomeAbout'
 
 const Index = () => {
   return (
     <div>
     <Hero/>
+    <HomeAbout/>
     </div>
   )
 }
