b4d5b3953953398457c9d103b79d5f5087cc9c48
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-15 19:13:06 +0400

    Next data finished

---
+++ client/src/api/GalleryD.jsx
@@ -0,0 +1,25 @@
+const GalleryD = [
+    {
+      cover: "https://rainbowdesign.in/themes/Stetho/img/partner/3.png",
+    },
+    {
+      cover: "https://rainbowdesign.in/themes/Stetho/img/partner/5.png",
+    },
+    {
+      cover: "https://rainbowdesign.in/themes/Stetho/img/partner/2.png",
+    },
+    {
+      cover: "https://rainbowdesign.in/themes/Stetho/img/partner/4.png",
+    },
+    {
+      cover: "https://rainbowdesign.in/themes/Stetho/img/partner/1.png",
+    },
+    {
+      cover: "https://rainbowdesign.in/themes/Stetho/img/partner/5.png",
+    },
+    {
+      cover: "https://rainbowdesign.in/themes/Stetho/img/partner/2.png",
+    },
+  ]
+  
+  export default GalleryD
