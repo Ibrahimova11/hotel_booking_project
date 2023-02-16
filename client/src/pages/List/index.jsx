f50ddbd6475eee1fa89f6fdd81763b5cc9bb67ab
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-17 02:06:22 +0400

    Date added to search table

--- client/src/pages/List/index.jsx
+++ client/src/pages/List/index.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 import "./style.css";
 const Index = () => {
+  
+    const location = useLocation();
+    const [destination, setDestination] = useState(location.state.destination);
+    const [date, setDate] = useState(location.state.date);
+    const [options, setOptions] = useState(location.state.options);
+  
   return (
     <div className="listContainer">
       <div className="listWrapper">
@@ -9,6 +18,13 @@ const Index = () => {
           <div className="listItem">
             <label>Destination</label>
             <input type="text" />
+          </div>
+          <div className="listItem">
+            <label>Check-in Date</label>
+            <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+              date[0].endDate,
+              "MM/dd/yyyy"
+            )}`}</span>
           </div>
         </div>
         <div className="listResult"></div>
