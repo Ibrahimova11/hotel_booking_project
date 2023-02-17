5e6ae52974a730664808f456ba202c5f10ea6ed5
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-17 20:22:56 +0400

    Hotel detail finished

--- client/src/components/SearchItem/index.jsx
+++ client/src/components/SearchItem/index.jsx
@@ -4,7 +4,7 @@ const Index = () => {
   return (
     <div className="searchItem">
       <img
-        src="https://tower-street-apartments-krakow.booked.net/data/Photos/OriginalPhoto/10070/1007001/1007001979/Tower-Street-Apartments-Krakow-Exterior.JPEG"
+        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
         alt=""
         className="siImg"
       />
