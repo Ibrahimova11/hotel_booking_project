46005491f86159cc45def53368e8e1de8ceb810c
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-21 20:54:18 +0400

    Admin folder structure created

---
+++ admin/src/datatablesource.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const datatablesource = () => {
+  return (
+    <div>datatablesource</div>
+  )
+}
+
+export default datatablesource
---
+++ admin/src/formSource.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const formSource = () => {
+  return (
+    <div>formSource</div>
+  )
+}
+
+export default formSource
